c9967310f39f48e49c3a0d75ec94045ac9618cdb
EC2 Default User <ec2-user@ip-172-31-74-202.ec2.internal>
2021-07-01 08:41:18 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6d9e81c5726447c7ec99429403901b4d85aa5a0772cd79301eedfc5e36779bdb7879bd5bb21168c8ca191c4f233fb4f3b141acf3bee5865c7c2a056ea49bd2a1'
---
+++ spec/models/list_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe "モデルに関するテスト" do
+  it "有効な投稿内容の場合は保存される" do
+    expect(FactoryBot.build(:list)).to be_valid
+  end
+end
